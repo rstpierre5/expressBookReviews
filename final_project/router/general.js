61166aa9e4c987d1f6c625d3043e4d3dec1f7530
Ryan <rstpierre517@gmail.com>
2023-01-30 19:37:52 -0500

    Added promises

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -25,33 +25,45 @@ public_users.post("/register", (req,res) => {
 
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
-    return res.status(200).send(JSON.stringify(books, null, 4));
+    let myPromise = new Promise((resolve,reject) => {
+        resolve(res.status(200).send(JSON.stringify(books, null, 4)));
+    });
+    myPromise.then(console.log("Sent books"));
 });
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
-    let isbn = req.params.isbn;
-    return res.status(200).send(JSON.stringify(books[isbn], null, 4));
+    let myPromise = new Promise((resolve,reject) => {
+        let isbn = req.params.isbn;
+        resolve(res.status(200).send(JSON.stringify(books[isbn], null, 4)));
+    });
+    myPromise.then(console.log("Sent books by isbn"));
  });
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
-    let author = req.params.author;
-    let details = Object.values(books);
-    let book = details.filter((item) =>{
-        return item.author === author;
+    let myPromise = new Promise((resolve,reject) => {
+        let author = req.params.author;
+        let details = Object.values(books);
+        let book = details.filter((item) =>{
+            return item.author === author;
+        });
+        resolve(res.status(200).send(JSON.stringify(book, null, 4)));
     });
-  return res.status(200).send(JSON.stringify(book, null, 4));
+    myPromise.then(console.log("Sent books by author"));
 });
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
-    let title = req.params.title;
-    let details = Object.values(books);
-    let book = details.filter((item) =>{
-        return item.title === title;
+    let myPromise = new Promise((resolve,reject) => {
+        let title = req.params.title;
+        let details = Object.values(books);
+        let book = details.filter((item) =>{
+            return item.title === title;
+        });
+    resolve(res.status(200).send(JSON.stringify(book, null, 4)));
     });
-  return res.status(200).send(JSON.stringify(book, null, 4));
+    myPromise.then(console.log("Sent books by title"));
 });
 
 //  Get book review
